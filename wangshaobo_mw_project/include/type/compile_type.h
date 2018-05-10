2f8e06fef610980e989042f1143b75f121c6a1b9
wangshaobo <jentlestea@zju.edu.cn>
2018-05-10 13:20:08 +0800

    删除可执行文件,使用make可以运行__GCC_C99 __TCPIP_TRANSMIT模式下的BC和RT，执行程序时先运行BC，再运行RT

--- wangshaobo_mw_project/include/type/compile_type.h
+++ wangshaobo_mw_project/include/type/compile_type.h
@@ -1,12 +1,14 @@
 #ifndef COMPILE_TYPE_H
 #define COMPILE_TYPE_H
-/*#define __GCC_C99 1
+#define __GCC_C99 1
 #define __TCPIP_TRANSMIT 2
 #define __RT_GCC_C99 1
 #define __RT_TCPIP_TRANSMIT 2
-*/
+
+/*
 #define __SPARC_GCC_MMU 2
 #define __VCAN_TRANSMIT 1
 #define __RT_GCC_MMU 2
 #define __RT_VCAN_TRANSMIT 1
+*/
 #endif
